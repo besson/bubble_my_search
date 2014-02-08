12f3fd0707905cf0212dec5043ef492f4a900574
Felipe Besson <flpbesson@gmail.com>
2014-02-08 18:55:58 -0200

    fixes search

--- routes/search.js
+++ routes/search.js
@@ -1,28 +1,38 @@
-var solr = require("solr-client");
 var HashMap = require("hashmap").HashMap;
 var map = new HashMap();
 var async = require("async");
-var config = require("../config.js")
+var request = require("request")
+var http = require("http")
+var JSONStream = require("JSONStream")
+var es = require("event-stream")
 
 exports.go = function(req, res){
   var keywords = (req.body.keywords).split(",");
-  var client = solr.createClient(config.solr.host, config.solr.port, config.solr.core, "/solr");
-
   var searches = 0;
 
   async.each(keywords,
     function(entry, e ) {
-      var query = client.createQuery().q(entry).rows(config.query.rows).fl(config.query.fl).sort(config.query.sort);
-      client.search(query,function(err,obj) {
-        console.log(obj.response.docs);
-        map.set(entry, obj.response.docs);
-        searches++;
+
+      var country_options = {
+        hostname: "api.worldbank.org",
+        port: 80,
+        path: "/countries/" + entry + "?format=json",
+        method: "GET"
+      }
+
+    http.get(country_options, function(response) {
+      response.pipe(JSONStream.parse("*.*")).pipe(es.mapSync(function(data) {
+
+       if (data.id) {
+          map.set(data.id, data);
+          searches++;
+       }
 
         if (searches == keywords.length) {
           res.render("search", { title: "results", results: map });
         }
 
-      });
+      }))})
       e();
     }
   );
